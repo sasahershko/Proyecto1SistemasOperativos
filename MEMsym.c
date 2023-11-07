0f2f7e2a59d7f0ce984c7b7a0e9ea42b984a3e14
Saray <sarayhershko@gmail.com>
2023-11-07 11:32:01 +0100

    Prueba desde consola Linux

--- MEMsym.c
+++ MEMsym.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	unsigned char ETQ;
+	unsigned char Data[TAM_LINEA]
+}T_CACHE_LINE;
+
+void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
+void VolcarCACHE(T_CACHE_LINE *tbl);
+void ParsearDireccion(unsigned int addr, int *ETQ, int*palabra, int *linea, int *bloque);
+void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ,int linea, int bloque);
 
 int main(){
 	printf("hola mundo");
