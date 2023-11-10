07eb47ead2e02e9633c992fdafa5a2a4529b4847
Ronambulo <101815831+Ronambulo@users.noreply.github.com>
2023-11-10 17:14:24 +0100

    Comentarios
    he hecho un par de comentarios para señalizar las distintas partes del codigo

--- MEMsym.c
+++ MEMsym.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//----------------------------------------------ESTRUCTURA---------------------------------------------------
+
 typedef struct{
 	unsigned char ETQ;
 	unsigned char Data[TAM_LINEA]
 }T_CACHE_LINE;
+
+//----------------------------------------------CONSTRUCTORES---------------------------------------------------
 
 void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
 void VolcarCACHE(T_CACHE_LINE *tbl);
 void ParsearDireccion(unsigned int addr, int *ETQ, int*palabra, int *linea, int *bloque);
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ,int linea, int bloque);
 
-int main(){
-	int num1 = 1;
-	int num2 = 2;
-	int suma = num1+num2;
+//----------------------------------------------MAIN---------------------------------------------------
 
-	printf("Hola buenas soy saray %d", suma);
+int main(){
+
 }
+
+//----------------------------------------------FUNCIONES---------------------------------------------------
 
 void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
 
 }
+
 void VolcarCACHE(T_CACHE_LINE *tbl){
 
 }
+
 void ParsearDireccion(unsigned int addr, int *ETQ, int*palabra, int *linea, int *bloque){
 
 }
+
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ,int linea, int bloque){
-	
+
 }
