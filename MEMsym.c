cd28efad81e74b56b26412b0feea963d7e802c21
Ronambulo <101815831+Ronambulo@users.noreply.github.com>
2023-11-10 17:10:34 +0100

    funciones

--- MEMsym.c
+++ MEMsym.c
@@ -18,3 +18,16 @@ int main(){
 
 	printf("Hola buenas soy saray %d", suma);
 }
+
+void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
+
+}
+void VolcarCACHE(T_CACHE_LINE *tbl){
+
+}
+void ParsearDireccion(unsigned int addr, int *ETQ, int*palabra, int *linea, int *bloque){
+
+}
+void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ,int linea, int bloque){
+	
+}
