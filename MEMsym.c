e5f8d6432207e535e7498fade6fe1e0576cf7305
Ronambulo <101815831+Ronambulo@users.noreply.github.com>
2023-11-14 20:18:37 +0100

    Arreglo de el merge?

--- MEMsym.c
+++ MEMsym.c
@@ -19,6 +19,8 @@ void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
 void VolcarCACHE(T_CACHE_LINE *tbl);
 void ParsearDireccion(unsigned int addr, int *ETQ, int*palabra, int *linea, int *bloque);
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ,int linea, int bloque);
+void MeterEnRAM(FILE* ficheroContRAM, unsigned char Simul_RAM[4096]);
+int FicherosExisten(FILE* f1, FILE* f2, FILE* f3);
 void CerrarFicheros(FILE* f1, FILE* f2, FILE* f3);
 
 //----------------------------------------------MAIN---------------------------------------------------
@@ -26,28 +28,51 @@ void CerrarFicheros(FILE* f1, FILE* f2, FILE* f3);
 int main(){
 	int globalTime = 0;
 	int numFallos = 0;
-	unsigned char Simul_RAM[4096];
+	
+	//Aquí se guarda lo que hay dentro del fichero .bin
+
 	FILE* ficheroDirMemoria = fopen("dirs_memoria.txt", "r");
 	FILE* ficheroContRAM = fopen("CONTENTS_RAM.bin", "r");
 	FILE* ficheroAcessoMemoria = fopen("accesos_memoria.txt", "r");
-	T_CACHE_LINE lineaCache;
 
-	LimpiarCACHE(lineaCache);
+	T_CACHE_LINE lineaCache[NUM_FILAS];
+	unsigned char Simul_RAM[4096];
 
-
-	//comprobación del fichero
-	if((ficheroDirMemoria == NULL)  || (ficheroContRAM == NULL) || (ficheroAcessoMemoria == NULL))
-	{
-		printf("Error: algun fichero no existe");
+	if(FicherosExisten(ficheroAcessoMemoria, ficheroContRAM, ficheroDirMemoria) == -1)
 		return(-1);
+
+	
+	LimpiarCACHE(lineaCache);
+	MeterEnRAM(ficheroContRAM, Simul_RAM);
+
+	unsigned long numBin = 0;
+	unsigned char TEMP = 0xff;
+	unsigned char numHex[3];
+	int contador = 0;
+	//leo el fichero accesos memoria
+	fopen("accesos_memoria.txt", "r");
+	
+	while((TEMP = getc(ficheroAcessoMemoria)) != '\n'){
+		numHex[contador]= TEMP;
+		contador++;
 	}
-	CerrarFicheros(ficheroAcessoMemoria, ficheroContRAM, ficheroDirMemoria);
+	numBin = strtoul(numHex, NULL, 16);
+	fclose(ficheroAcessoMemoria);
+	/*
+	- Cogemos la primera dirección : 22E
 
+	- Lo convertimos a binario, los separamos por bits(palabra, linea y etiqueta)
+			separamos los ultimos bits (5) de la etiqueta
 
+	- lo comparamos con la estructura
+	- Si da error se incremeta fallo +1 y tiempo +20, se copia el bloque de de Simul_RAM en la caché y se imprime lo que se esta cargando en la caché
+	
+	*/	
+	
 	//ESTO SE TIENE QUE REPETIR
 	//MIRAR DESPLAZAMIENTOS Y NUMEROS DE BITS EN C
 
-	if(lineaCache.ETQ == TAM_LINEA)
+	/*if(lineaCache[0].ETQ == TAM_LINEA)
 	{
 
 	}
@@ -56,8 +81,8 @@ int main(){
 		numFallos++;
 		printf("T: %d, Fallo de CACHE %d, ADDR %04X Label %X linea %02Xpalabra %02X bloque %02X", globalTime, numFallos);
 		globalTime += 20;
-	}
-	//HASTA AQUI
+	}*/
+	//HASTA AQUÍ
 
 
 	return 0;
@@ -65,28 +90,58 @@ int main(){
 
 //----------------------------------------------FUNCIONES---------------------------------------------------
 
+// Esta función sirve para limpiar todas los valores de la cache, tanto de las etiquetas como de los datos.
 void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
+	for(int i =0; i<NUM_FILAS;i++){
 
-	lineaCache.ETQ = 0xFF;
-	for(int i = 0; i < TAM_LINEA; i++)
-	{
-			lineaCache.Data[i] = 0x23;
+		tbl[i].ETQ = 0xFF;
+		
+		for(int j = 0; j < TAM_LINEA; j++)
+		{
+			tbl[i].Data[j] = 0x23;
+		}
 	}
-
 }
 
-void VolcarCACHE(T_CACHE_LINE *tbl){
 
+//Antes de acabar el programa, vuelca los contenidos dentro del fichero .bin
+void VolcarCACHE(T_CACHE_LINE *tbl){
 }
 
 void ParsearDireccion(unsigned int addr, int *ETQ, int*palabra, int *linea, int *bloque){
-
 }
 
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ,int linea, int bloque){
-
 }
 
+
+void MeterEnRAM(FILE* ficheroContRAM, unsigned char Simul_RAM[4096]){
+	char input = '\0';
+	int cont = 0;
+
+	fopen("CONTENTS_RAM.bin", "rb");
+	while((input = getc(ficheroContRAM)) != '\0' && !(feof(ficheroContRAM))){
+		Simul_RAM[cont] = input;
+		printf("%c", Simul_RAM[cont]);
+		//printf("i: %d", cont);	
+		cont++;
+	}
+	fclose(ficheroContRAM);
+}
+
+//Comprobación del ficheros
+int FicherosExisten(FILE* f1, FILE* f2, FILE* f3){
+	
+	if((f1 == NULL)  || (f2 == NULL) || (f3 == NULL))
+	{
+		printf("Error: algun fichero no existe");
+		return(-1);
+	}
+	return 0;
+	CerrarFicheros(f1,f2,f3);
+}
+
+//Cierre de ficheros
 void CerrarFicheros(FILE* f1, FILE* f2, FILE* f3){
 	fclose(f1);
 	fclose(f2);
