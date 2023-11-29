6fcf2055a7855dc42b197b076e4501322f75716b
Ronambulo <101815831+Ronambulo@users.noreply.github.com>
2023-11-29 16:48:39 +0100

    Solo falta lo del fichero
    He impreso lo de la palabra en el main

--- MEMsym.c
+++ MEMsym.c
@@ -20,6 +20,7 @@ void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
 void VolcarCACHE(T_CACHE_LINE *tbl);
 void ParsearDireccion(unsigned int addr, int *ETQ, int*palabra, int *linea, int *bloque);
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ,int linea, int bloque);
+char guardarValor(T_CACHE_LINE *tbl, int palabra, int linea);
 void MeterEnRAM(FILE* ficheroContRAM, unsigned char Simul_RAM[4096]);
 int FicherosExisten(FILE* f1, FILE* f2, FILE* f3);
 void CerrarFicheros(FILE* f1, FILE* f2, FILE* f3);
@@ -33,7 +34,8 @@ int main(){
 
 	unsigned int Hex;
 	int ETQ=0, palabra=0, linea=0, bloque=0;
-	int contAccesos = 0;
+	int contAccesos = 0, index = 0;
+	char textoRamPalabra[16];
 	
 	//Aquí se guarda lo que hay dentro del fichero .bin
 
@@ -58,15 +60,17 @@ int main(){
 	LimpiarCACHE(lineaCache);
 	while((fscanf(ficheroAcessoMemoria,"%X",&Hex)!=EOF)){
 		ParsearDireccion(Hex, &ETQ, &palabra, &linea, &bloque);	
-
 		if(ETQ != lineaCache[linea].ETQ){
 			numFallos++;
 			globalTime += 20;
-			printf("\n\nHa habido un fallo: fallo nº : %d\n", numFallos);
+			printf("\n\nHa habido un fallo: fallo n: %d\n", numFallos);
 			printf("T: %d, Fallo de CACHE %d, ADDR %04X ETQ %X linea %02X palabra %02X bloque %02X\n\n", globalTime, numFallos, Hex, ETQ, linea, palabra, bloque);
 			TratarFallo(lineaCache, Simul_RAM, ETQ, linea, bloque);
 		}
+
 		//hacer lo del array
+		textoRamPalabra[index] = guardarValor(lineaCache, palabra, linea);
+		index++;
 		printf("\nT: %d, Acierto de CACHE, ADDR %04X ETQ %X linea %02X palabra %02X bloque %02X\n\n", globalTime, Hex, ETQ, linea, palabra, bloque);
 		for(int i = 0; i < NUM_FILAS; i++){
 			printf("%X\tDatos: ", lineaCache[i].ETQ);
@@ -76,11 +80,16 @@ int main(){
 			printf("\n");
 		}
 		contAccesos++;
-		//sleep(1);
+		sleep(1);
+
+		
 	}
 	printf("\n\n\n");
 	printf("Accesos totales: %d; Fallos %d; Tiempo Medio %f\n\n", contAccesos, numFallos, (float)(globalTime/contAccesos));
-	VolcarCACHE(lineaCache); 
+	VolcarCACHE(lineaCache);
+	for(int i = 0; i <= index; i++){
+		printf("%c", textoRamPalabra[i]);
+	} 
 	fclose(ficheroContRAM);
 
 }
@@ -103,13 +112,13 @@ void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
 
 //Antes de acabar el programa, vuelca los contenidos dentro del fichero .bin
 void VolcarCACHE(T_CACHE_LINE *tbl) {
-    for (int i = 0; i < NUM_FILAS; i++) {
-        //printf("%X", tbl[i].ETQ);
-        for (int j = 0; j < TAM_LINEA; j++) {
-        printf("%c", tbl[i].Data[j]);
-        }
-        printf("\n");
-    }
+    // for (int i = 0; i < NUM_FILAS; i++) {
+    //     //printf("%X", tbl[i].ETQ);
+    //     for (int j = 0; j < TAM_LINEA; j++) {
+    //     printf("%c", tbl[i].Data[j]);
+    //     }
+    //     printf("\n");
+    // }
 }
 
 
@@ -142,6 +151,10 @@ void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ,int linea, int bloque){
 		tbl[linea].Data[i] = MRAM[i + j];
 }
 
+char guardarValor(T_CACHE_LINE *tbl, int palabra, int linea){
+	return tbl[linea].Data[palabra];
+}
+
 void MeterEnRAM(FILE* ficheroContRAM, unsigned char Simul_RAM[4096]){
 	char input = '\0';
 	int cont = 0;
@@ -150,7 +163,7 @@ void MeterEnRAM(FILE* ficheroContRAM, unsigned char Simul_RAM[4096]){
 	while((input = getc(ficheroContRAM)) != '\0' && !(feof(ficheroContRAM))){
 		Simul_RAM[cont] = input;
 		cont++;
-		printf("%c", Simul_RAM[cont]);
+		//printf("%c", Simul_RAM[cont]);
 	}
 	fclose(ficheroContRAM);
 }
